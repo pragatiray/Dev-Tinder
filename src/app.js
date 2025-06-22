3084dba32b5d8f1c92119d92cc2fae316644b8b5
pragatiray <93862662+pragatiray@users.noreply.github.com>
2025-06-23 01:20:55 +0545

    Added validator and sanitized the data

--- src/app.js
+++ src/app.js
@@ -1,17 +1,62 @@
 const express = require("express");
 const { connectDB, User } = require("./config/database");
 const app = express();
+app.use(express.json())
 
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName : "Virat",
-    lastName : "Kholi",
-    emailId : "virat@kholi.com",
-    password : "samaira"
-  });
-  await user.save();
-  res.send("User saved successfully!");
+  const user = new User(req.body);
+  try {
+     await user.save();
+     res.send("User saved successfully!");
+  } catch (err) {
+     res.status(400).send("Error saving the data: " + err.message);
+  }
 });
+app.get("/user",async(req,res) => {
+  const userEmail = req.body.emailId;
+  try{
+  const user = await User.findOne({emailId:userEmail})
+  res.send(user)
+  } catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+app.get("/feed",async(req,res) => {
+  try{
+  const user = await User.find({})
+  res.send(user)
+  } catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+app.delete("/user",async(req,res) =>{
+  const userId = req.body.userId;
+  try{
+    const user = await User.findByIdAndDelete({_id: userId})
+    res.send("User added successfully")
+  } catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+app.patch("/user/:userId", async(req,res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try{
+  const ALLOWED_UPDATES = ["userId","photoUrl","about","gender","age", "skills"]
+  const isUpdateAllowed = Object.keys(data).every((k) =>
+    ALLOWED_UPDATES.includes(k)
+  );
+  if(!isUpdateAllowed){
+   throw new Error("Update not allowed")
+  }
+    await User.findByIdAndUpdate({_id: userId},data)
+    res.send("User updated successfully")
+  } catch(err){
+    res.status(400).send("Something went wrong")
+  }
+})
+
 connectDB()
   .then(() => {
     app.listen(7777, () => {
@@ -20,4 +65,4 @@ connectDB()
   })
   .catch((err) => {
     console.error("Failed to connect to MongoDB:", err.message);
-  });
+});
