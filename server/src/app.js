37543eca4897cdc8776b9d35a3895fcc5b8af0e2
Pragati <prags@Pragatis-MacBook-Air.local>
2026-04-11 14:30:16 +0545

    Resolved errors

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/database");
+const {connectDB} = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 
@@ -8,7 +9,7 @@ app.use(cookieParser());
 
 const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
-const requestRouter = require("./routes/requests");
+const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
 
 app.use("/", authRouter);
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-// const validator = require("validator");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/testDB")
-      console.log("Connected to MongoDB");
-   } catch (err) {
-    console.log("MogoDB connection error:", err.message);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log("MongoDB connection error:", err.message);
+    process.exit(1); // stop app if DB fails
   }
 };
 
