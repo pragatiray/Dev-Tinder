300b61edfa7c3ea7894372932a740ee87d35714b
Pragati <prags@Pragatis-MacBook-Air.local>
2026-04-12 15:14:47 +0545

    Added MONGO_URL directly in the database file to test in AWS server

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect("mongodb+srv://pragatiray445_db_user:wP68pDeMvqxHamxr@task-management.be4qgxv.mongodb.net/");
     console.log("Connected to MongoDB");
   } catch (err) {
     console.log("MongoDB connection error:", err.message);
