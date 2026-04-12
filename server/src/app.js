a19f0b54762f390ea8f4d52ce825d0f5389e38e1
Pragati <prags@Pragatis-MacBook-Air.local>
2026-04-12 15:34:49 +0545

    Fixed small error

--- server/src/app.js
+++ server/src/app.js
@@ -30,7 +30,7 @@ connectDB()
   .then(() => {
     console.log("database connection established");
     app.listen(7777, () => {
-      console.log("Server is successfully lisrenning on port 3000....");
+      console.log("Server is successfully listening on port 7777....");
     });
   })
   .catch((err) => {
